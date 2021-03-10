e70f574069912b81854f3c526f0809c1f3b49d2e
Amanda Cavallaro <amdcaruso@gmail.com>
2021-03-10 20:56:12 +0000

    Refactors code

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Terms and Conditions Formatter
 Terms at times can be given in different formats: e.g 1. 2) as we are not aware which format will be given to us, to transform a text terms & conditions file into a html format we can wrap paragraph tags around it.
+
 This tool works by adding the text to the left textarea and the results will be seen real time on the textarea to your right.
 This was built with HTML, CSS and JavaScript.
+
+Demo (here)(https://termsformatter.netlify.app/).
+
+Icon credit:
+<div>Icon made by <a href="https://www.flaticon.com/authors/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
 function transformTerms() {
-    const str = document.getElementById("initialTextArea").value;
+    const str = document.getElementById('initialTextArea').value;
     /* Regular expression */
     const re = /(.*\S)/gi;
     /* The p tags to be added around each line*/
-    const subst = "<p>$1</p>";
+    const subst = '<p>$1</p>';
     /* Adds regex to the variable that contains the initial text area
     and saves it on the variable result*/
     const result = str.replace(re, subst);
-    /* The Result Text Area receives the formatted text */
-    document.getElementById("resultTextArea").innerHTML = result;
-  }
+    /* Adds the formatted text to the Result Text Area */
+    document.getElementById('resultTextArea').innerHTML = result;
+}
 
-  function copyToClipboard() {
+function copyToClipboard() {
     /* Gets the text field */
-    const textToBeCopied = document.getElementById("resultTextArea");
+    const textToBeCopied = document.getElementById('resultTextArea');
     /* Selects the textarea */
     textToBeCopied.select();
-    /* For mobile devices */
+    /* Sets the start and end positions of the current text selection for mobile devices */
     textToBeCopied.setSelectionRange(0, 99999);
     /* Executes command to copy the text inside the textarea */
-    document.execCommand("copy");
-  }
+    document.execCommand('copy');
+}
